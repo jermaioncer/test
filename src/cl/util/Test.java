9a68babf93a04c7b4021323e51f490704041574e
jermaioncer <germancer@gmail.com>
2011-02-06 23:02:05 -0300

    sdfgh

--- src/cl/util/Test.java
+++ src/cl/util/Test.java
@@ -3,6 +3,6 @@ package cl.util;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hola a todos 2");
-		System.out.println("Hola a todos");
+		System.out.println("Hola a todos...");
 	}
 }
